1c6fafb454461d709ad3bfe3fa97b7830105d6ae
gabriel-victor933 <gabrielvictor933@usp.br>
2023-03-15 20:05:51 -0300

    footer

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -1,8 +1,62 @@
+import styled from "styled-components"
+import {Link} from "react-router-dom"
+import { CircularProgressbar,buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 const Footer = () => {
 
     return (
-        <div>Footer</div>
+        <Menu>
+            <Link to="/">Hábitos</Link>
+            <Link to="/" className="progress-bar">
+                <CircularProgressbar
+                    value={50}
+                    text={"hoje"}
+                    background
+                    backgroundPadding={6}
+                    styles={buildStyles({
+                    backgroundColor: "#3e98c7",
+                    textColor: "#fff",
+                    pathColor: "#fff",
+                    trailColor: "transparent",
+                    textSize: "18px",
+                    textColor: "#fff"
+                    })}
+                ></CircularProgressbar>
+            </Link>
+            
+            <Link to="/">Histórico</Link>
+        </Menu>
     )
 }
 
 export default Footer
+
+const Menu = styled.div`
+    width: 100%;
+    height: 70px;
+    background-color: #ffffff;
+    position: fixed;
+    bottom: 0px;
+    left: 0px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    a {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 22px;
+        text-align: center;
+        text-decoration: none;
+        color: #52B6FF;
+    }
+
+    .progress-bar {
+        width: 91px;
+        height: 91px;
+        color: white;
+        margin-bottom: 45px;
+    }
+`;
